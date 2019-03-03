15fc0d7e6ef7ea0e80145fdaf9ea3635b86afa65
Noah Hummel <n.p.hummel@gmail.com>
2019-03-03 11:10:59 +0100

    [FEATURE] ADD db connector

---
+++ app/src/db.js
@@ -0,0 +1,20 @@
+import * as R from "ramda";
+import * as Future from "fluture";
+import * as MongoDb from "mongodb";
+
+function db(version) {
+    return Future.Future((reject, resolve) => {
+        const client = new MongoDb.MongoClient(`mongodb://mongodb:27017/${version}`);
+        client.connect(err => {
+            if (!R.isNil(err)) {
+                reject(err);
+            } else {
+                resolve(client.db(version));
+            }
+        });
+    });
+}
+
+export {
+    db
+}
