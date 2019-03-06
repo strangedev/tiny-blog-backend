c7ba5de4a0b7df37ac7a5f5c1045d45da9883edb
Noah Hummel <n.p.hummel@gmail.com>
2019-03-06 15:07:34 +0100

    [ADD] Public/BlogEntry.view.byTag

--- app/src/router/Public/BlogEntry.js
+++ app/src/router/Public/BlogEntry.js
@@ -8,17 +8,39 @@ let router = express.Router();
 router.use(function limitAndOffsetDefaults(req, res, next) {
     if (R.isNil(req.query.limit)) {
         req.query.limit = 50;
+    } else {
+        req.query.limit = parseInt(req.query.limit);
     }
     if (R.isNil(req.query.offset)) {
         req.query.offset = 0;
+    } else {
+        req.query.offset = parseInt(req.query.offset);
     }
     next();
 });
 
 router.use(express.json());
 
-router.get('/byTag', function(req, res) {
-    res.send("Not implemented");
+router.post('/byTag', function(req, res) {
+    const db = store("localhost", 27017);
+    if (R.isNil(req.body.tags)) {
+        res.sendStatus(400);
+    } else {
+        db.BlogEntry.view.byTag(
+            req.body.tags,
+            req.query.offset,
+            req.query.limit
+        ).map(
+            R.partial(R.map, [blogEntry => blogEntry.marshal()])
+        ).fork(
+            console.error,
+            results => {
+                res.format({
+                    json: () => res.send(results)
+                });
+            }
+        );
+    }
 });
 
 router.get('/newest', function(req, res) {
